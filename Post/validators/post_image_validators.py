cd5893ab5ee832850c91393797c2b9ebcc2384ea
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-05-18 22:26:54 +0430

    Bug fixed image validators

--- Post/validators/post_image_validators.py
+++ Post/validators/post_image_validators.py
@@ -1,12 +1,15 @@
 import PIL.Image
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from filetype import filetype
 
 
 def post_image_validate(image):
+    if type(image) is InMemoryUploadedFile:
+        return
     kind = filetype.guess(image)
-    if not kind.extension and kind.extension not in settings.POST_ALLOWED_IMAGE_EXTENSIONS:
+    if not kind or kind.extension not in settings.POST_ALLOWED_IMAGE_EXTENSIONS:
         raise ValidationError(
             settings.ERROR_MESSAGES['POST_IMAGE_FORMAT_INVALID']
         )
