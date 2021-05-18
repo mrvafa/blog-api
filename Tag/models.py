9da90e13e1f57cfa0aa38b0197ecf70185cd26b4
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-05-18 21:32:13 +0430

    Change image path tag for media

--- Tag/models.py
+++ Tag/models.py
@@ -9,7 +9,7 @@ from Tag.validators.tag_image_validators import tag_image_validate
 class Tag(models.Model):
     title = models.CharField(max_length=settings.TAG_TITLE_LENGTH_MAX, unique=True, )
     slug = models.CharField(max_length=settings.TAG_TITLE_LENGTH_MAX, unique=True, editable=False)
-    image = models.ImageField(upload_to='tag/%d', validators=[tag_image_validate,], blank=True, null=True, )
+    image = models.ImageField(upload_to='tag/%m', validators=[tag_image_validate,], blank=True, null=True, )
     body = RichTextUploadingField(blank=True, null=True)
     added_datetime = models.DateTimeField(auto_created=True, blank=True, null=True, )
     modify_datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True, )
