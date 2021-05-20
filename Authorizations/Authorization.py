ee1c5a706e8ca470479909eb4759d3ddd02a8ff3
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-05-20 09:59:03 +0430

    Authorization bug fixed

--- Authorizations/Authorization.py
+++ Authorizations/Authorization.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import BasePermission
 
 from Post.models import Post
@@ -6,27 +7,29 @@ from Profile.models import Profile
 
 class IsAuthorOfThisPost(BasePermission):
     def has_permission(self, request, view):
-        if not Post.objects.filter(id=view.kwargs['pk']).first():
+        if not request.user or isinstance(request.user, AnonymousUser):
             return False
         return bool(
-            request.user and request.user == Post.objects.filter(
+            Post.objects.filter(id=view.kwargs['pk']).first() and
+            request.user == Post.objects.filter(
                 id=view.kwargs['pk']).first().author
         )
 
 
 class IsAuthor(BasePermission):
     def has_permission(self, request, view):
-        if not Profile.objects.filter(user=request.user).first():
+        if not request.user or isinstance(request.user, AnonymousUser):
             return False
         return bool(
-            request.user and
+            Profile.objects.filter(user=request.user).first() and
             Profile.objects.filter(user=request.user).first().position == 'au'
         )
 
 
 class IsAdminOrAuthorOfThisPost(BasePermission):
     def has_permission(self, request, view):
-        if not Post.objects.filter(id=view.kwargs['pk']).first():
+        if not request.user or isinstance(request.user, AnonymousUser):
             return False
-        return bool(request.user and request.user.is_staff) or bool(
-            request.user and request.user == Post.objects.filter(id=view.kwargs['pk']).first().author)
+        return bool(Post.objects.filter(id=view.kwargs['pk']).first() and
+                    request.user.is_staff) or bool(
+            request.user == Post.objects.filter(id=view.kwargs['pk']).first().author)
