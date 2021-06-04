cdfc0abb00cdf8121f38f18bc45b1837bbcfa39b
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-04 10:31:02 +0430

    Added test for listing tag and post

---
+++ Post/api/tests/list_of_posts.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.contrib.auth.models import Permission
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+import requests
+
+from MyUser.models import User
+from Post.models import Post
+
+
+class TestListOfPosts(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        height = settings.POST_IMAGE_WIDTH_MIN
+        image_content = requests.get(f'https://picsum.photos/{height}').content
+        self.image = SimpleUploadedFile(
+            name='test_post',
+            content_type='image/jpeg',
+            content=image_content,
+        )
+        self.user = User.objects.create(username='user')
+        author_permission = Permission.objects.get(codename='is_author')
+        self.user.user_permissions.add(author_permission)
+        self.post_1 = Post.objects.create(title='p1', image=self.image, body='b1', author=self.user)
+        self.post_2 = Post.objects.create(title='p2', image=self.image, body='b2', author=self.user)
+        self.post_3 = Post.objects.create(title='p3', image=self.image, body='b3', author=self.user)
+
+    def test_ok_list_of_tags(self):
+        respond = self.client.get(reverse('post_list'))
+        self.assertEqual(200, respond.status_code)
+
+    def test_ok_list_of_tags_check_ordering(self):
+        respond = self.client.get(reverse('post_list'))
+        users = respond.json()['results']
+        self.assertEqual('p3', users[0]['title'])
+        self.assertEqual('p2', users[1]['title'])
+        self.assertEqual('p1', users[2]['title'])
---
+++ Tag/api/tests/list_of_tags.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from Tag.models import Tag
+
+
+class TestListOfTags(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.tag_1 = Tag.objects.create(title='t1')
+        self.tag_2 = Tag.objects.create(title='t2')
+        self.tag_3 = Tag.objects.create(title='t3')
+
+    def test_ok_list_of_tags(self):
+        respond = self.client.get(reverse('tag_list'))
+        self.assertEqual(200, respond.status_code)
+
+    def test_ok_list_of_tags_check_ordering(self):
+        respond = self.client.get(reverse('tag_list'))
+        users = respond.json()['results']
+        self.assertEqual('t3', users[0]['title'])
+        self.assertEqual('t2', users[1]['title'])
+        self.assertEqual('t1', users[2]['title'])
