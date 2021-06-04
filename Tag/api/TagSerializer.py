0f8ee2386409837a42a88b1c83573311463040ca
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-04 19:36:25 +0430

    Make slug, added_datetime and modify_datetime to readonly

--- Tag/api/TagSerializer.py
+++ Tag/api/TagSerializer.py
@@ -1,9 +1,14 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from Tag.models import Tag
 
 
 class TagSerializer(ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+    added_datetime = serializers.DateTimeField(read_only=True)
+    modify_datetime = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Tag
         fields = ('title', 'image', 'body', 'added_datetime', 'modify_datetime', 'slug')
