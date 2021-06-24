b4104432cf5d3d3b52ecf6f6f5bae10f182cf6ce
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-24 20:55:11 +0430

    renamed variable for in test

--- Tag/api/tests/list_of_tags.py
+++ Tag/api/tests/list_of_tags.py
@@ -18,10 +18,10 @@ class TestListOfTags(TestCase):
 
     def test_ok_list_of_tags_check_ordering(self):
         respond = self.client.get(reverse('tag_list'))
-        users = respond.json()['results']
-        self.assertEqual('t3', users[0]['title'])
-        self.assertEqual('t2', users[1]['title'])
-        self.assertEqual('t1', users[2]['title'])
+        tags = respond.json()['results']
+        self.assertEqual('t3', tags[0]['title'])
+        self.assertEqual('t2', tags[1]['title'])
+        self.assertEqual('t1', tags[2]['title'])
 
     def test_ok_search(self):
         respond = self.client.get(reverse('tag_list') + '?search=t1')
