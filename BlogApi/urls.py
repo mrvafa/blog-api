745dec1c0ceb6be0bcae1251e0c7df6a1d83b20c
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-07-02 18:34:26 +0430

    Change some urls and added to README.MD

--- BlogApi/urls.py
+++ BlogApi/urls.py
@@ -1,22 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_auth.registration.views import VerifyEmailView
-from rest_auth.views import (PasswordResetView, PasswordResetConfirmView, LoginView, UserDetailsView,
-                             PasswordChangeView, )
+from rest_auth.views import (PasswordResetView, PasswordResetConfirmView, LoginView, UserDetailsView,)
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  re_path(r'^rest-auth/password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
-                  re_path(r'^rest-auth/password/reset/confirm/$', PasswordResetConfirmView.as_view(),
+                  re_path(r'^register/', include('rest_auth.registration.urls')),
+                  re_path(r'^password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
+                  re_path(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
                           name='rest_password_reset_confirm'),
-                  re_path(r'^rest-auth/login/$', LoginView.as_view(), name='rest_login'),
-                  re_path(r'^rest-auth/user/$', UserDetailsView.as_view(), name='rest_user_details'),
-                  re_path(r'^rest-auth/password/change/$', PasswordChangeView.as_view(),
-                          name='rest_password_change'),
-                  path('rest-auth/registration/', include('rest_auth.registration.urls')),
-                  re_path(r'^account-confirm-email$', VerifyEmailView.as_view(),
+                  re_path(r'^login/$', LoginView.as_view(), name='rest_login'),
+                  re_path(r'^profile/$', UserDetailsView.as_view(), name='rest_user_details'),
+                  re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(),
                           name='account_email_verification_sent'),
                   re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(),
                           name='account_confirm_email'),
