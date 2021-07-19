82aa2c742c4a96898c69dab6cc97a3bad7af306e
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-07-19 15:52:50 +0430

    fixed status

--- BlogApi/Settings/static.py
+++ BlogApi/Settings/static.py
@@ -1,4 +1,8 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
+
+from django.conf import settings
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(settings.BASE_DIR), 'static')
--- BlogApi/urls.py
+++ BlogApi/urls.py
@@ -1,24 +1,27 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import url
+import django.views.static
 from django.urls import path, include, re_path
 from rest_auth.registration.views import VerifyEmailView
-from rest_auth.views import (PasswordResetView, PasswordResetConfirmView, LoginView, UserDetailsView,)
+from rest_auth.views import (PasswordResetView, PasswordResetConfirmView, LoginView, UserDetailsView, )
 
 urlpatterns = [
-                  re_path(r'^register/', include('rest_auth.registration.urls')),
-                  re_path(r'^password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
-                  re_path(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
-                          name='rest_password_reset_confirm'),
-                  re_path(r'^login/$', LoginView.as_view(), name='rest_login'),
-                  re_path(r'^profile/$', UserDetailsView.as_view(), name='rest_user_details'),
-                  re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(),
-                          name='account_email_verification_sent'),
-                  re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(),
-                          name='account_confirm_email'),
-                  path('ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^register/', include('rest_auth.registration.urls')),
+    re_path(r'^password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
+    re_path(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
+            name='rest_password_reset_confirm'),
+    re_path(r'^login/$', LoginView.as_view(), name='rest_login'),
+    re_path(r'^profile/$', UserDetailsView.as_view(), name='rest_user_details'),
+    re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(),
+            name='account_email_verification_sent'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(),
+            name='account_confirm_email'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 
-                  path('post/', include('Post.urls')),
-                  path('tag/', include('Tag.urls')),
-                  path('profile/', include('MyUser.urls')),
+    path('post/', include('Post.urls')),
+    path('tag/', include('Tag.urls')),
+    path('profile/', include('MyUser.urls')),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^static/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG}),
+]
